07f8da676c7f5db7832bec76f2c51c892be64f83
Victor Zapata <victorzapatabedoya@hotmail.com>
2021-01-12 17:20:57 -0500

    Updated ProductApplicationTests

--- src/test/java/co/magnifik/demo/product/ProductApplicationTests.java
+++ src/test/java/co/magnifik/demo/product/ProductApplicationTests.java
@@ -1,13 +1,21 @@
 package co.magnifik.demo.product;
 
+import co.magnifik.demo.product.rest.controller.ProductController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ProductApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ProductController controller;
+
+    @Test
+    void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
 
 }
